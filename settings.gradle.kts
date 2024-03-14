00e86dac55c799487c78647d883eb8bc4b0135ba
tkDev <153438920+tkDev0101@users.noreply.github.com>
2024-03-14 13:28:12 +0200

    created Project and placed in repo

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Firebase-Authentication- App-tk4"
+include(":app")
+ 
